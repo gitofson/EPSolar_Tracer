74b71de3a24fb018848887fbb907bf26887550ee
Pavel Stemberk <stemberk@gmail.com>
2023-01-31 20:37:41 +0100

    tracer.py - 4bit power fix

--- tracer.py
+++ tracer.py
@@ -4,10 +4,14 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from time import sleep
 from sys import argv
 from db import db
+import struct
 
 client = ModbusClient(method = 'rtu', port = '/dev/ttyXRUSB0', baudrate = 115200)
 client.connect()
 print(len(argv))
+def getFloat(bits):
+    s = struct.pack('>L',bits)
+    return struct.unpack('>f', s)[0]
 if len(argv) > 1:
     devId = int(argv[1])
 else:
@@ -20,10 +24,10 @@ if True:
     dat['solarCurrent'] = float(result.registers[1] / 100.0)
     dat['batteryVoltage'] = float(result.registers[4] / 100.0)
     dat['chargeCurrent'] = float(result.registers[5] / 100.0)
-    dat['chargePower'] = float((result.registers[7]*100 + result.registers[6])/ 100.0)
+    dat['chargePower'] = float(((result.registers[7]<<16) + result.registers[6])/ 100.0)
     dat['loadVoltage'] = float(result.registers[12] / 100.0)
     dat['loadCurrent'] = float(result.registers[13] / 100.0)
-    dat['loadPower'] = float((result.registers[15]*100 + result.registers[14])/ 100.0)
+    dat['loadPower'] = float(((result.registers[15]<<16) + result.registers[14])/ 100.0)
    # dat['remoteBatteryTemp'] = float(result.registers[27] / 100.0)
     
     result = client.read_input_registers(0x3300,31,unit=devId)
@@ -32,9 +36,9 @@ if True:
     dat['maxBatteryVoltageToday'] = float(result.registers[2] / 100.0)
     dat['minBatteryVoltageToday'] = float(result.registers[3] / 100.0)
     dat['generatedEnergyToday'] = float((result.registers[13]*100 + result.registers[12]) / 100.0)
-    dat['generatedEnergyThisMonth'] = float((result.registers[15]*100 + result.registers[14]) / 100.0)
-    dat['generatedEnergyThisYear'] = float((result.registers[17]*100 + result.registers[16]) / 100.0)
-    dat['generatedEnergyTotal'] = float((result.registers[19]*100 + result.registers[18]) / 100.0)
+    dat['generatedEnergyThisMonth'] = float(((result.registers[15]<<16) + result.registers[14]) / 100.0)
+    dat['generatedEnergyThisYear'] = float(((result.registers[17]<<16) + result.registers[16]) / 100.0)
+    dat['generatedEnergyTotal'] = float(((result.registers[19]<<16) + result.registers[18]) / 100.0)
  
     # Do something with the data
     for (k,v) in dat.items():
