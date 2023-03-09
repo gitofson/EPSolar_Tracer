1e9f5f398bd0d63064342c894a375310a3c03c20
Pavel Stemberk <stemberk@gmail.com>
2023-03-09 08:56:55 +0100

    influx2 +

--- db.py
+++ db.py
@@ -1,21 +1,27 @@
-from influxdb import InfluxDBClient
-from influxdb import exceptions
+from influxdb_client import InfluxDBClient
+#from influxdb_client import InfluxDBClientError
 
 class db:
     dbname = ['pv_main']
+    #dbname = ['buck01']
+    url='http://myurl.org:8086'
+    org = 'My Organiyation l.t.d'
+    token='F7my_token_._____my_token________________my_token_________________my_token____________=='
     @staticmethod
     def dbsave(measurement, rawdat, defSeriesName='brushEvents'):
         mlist=db.dbname+[measurement]
-        client = InfluxDBClient(host='elektrovit.cz', port=8086)
+        client = InfluxDBClient(url=db.url, org=db.org, token=db.token)
+        write_api = client.write_api()
         for dbname in mlist:
             #client.create_database(dbname)
             try:
-                client.switch_database(dbname)
-                client.write_points([
+                #client.switch_database(dbname)
+                write_api.write(bucket=dbname+'/autogen', org=db.org, record= [
                         {
                         "measurement": measurement if dbname == 'pv_main' else defSeriesName,
                         "fields": rawdat
                         }
                 ])
-            except  exceptions.InfluxDBClientError:
-                pass
+            except:
+                print("DB Error "+dbname)
+        write_api.close()
